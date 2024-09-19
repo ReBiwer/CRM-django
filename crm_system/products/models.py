6db23715980fdcd69107400d5b52d5329a06b361
vladimir <vovka1998@gmail.com>
2024-09-19 17:45:17 +0300

    added PermissionOperator and updated Product (added attribute created_by)

--- crm_system/products/models.py
+++ crm_system/products/models.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 
 from django.db import models
+from django.contrib.auth.models import User
 from django.db.models import Manager
 
 
@@ -8,6 +9,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     cost = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     if TYPE_CHECKING:
         objects: Manager
--- crm_system/products/views.py
+++ crm_system/products/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic import DetailView, CreateView, DeleteView, UpdateView, ListView
 from django.urls import reverse_lazy, reverse
 from django.template import RequestContext
@@ -5,31 +6,44 @@ from django.template import RequestContext
 from .models import Product
 
 
-class CreateProductView(CreateView):
+class PermissionOperator(PermissionRequiredMixin):
+    permission_required = [
+        'products.add_product',
+        'products.change_product',
+        'products.delete_product',
+        'products.view_product'
+    ]
+
+
+class CreateProductView(PermissionOperator, CreateView):
     model = Product
     fields = '__all__'
     template_name = 'products/products-create.html'
     success_url = reverse_lazy('products:products')
 
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
 
-class DetailProductView(DetailView):
+
+class DetailProductView(PermissionOperator, DetailView):
     model = Product
     template_name = 'products/products-detail.html'
 
 
-class ListProductView(ListView):
+class ListProductView(PermissionOperator, ListView):
     queryset = Product.objects.all()
     context_object_name = 'products'
     template_name = 'products/products-list.html'
 
 
-class DeleteProductView(DeleteView):
+class DeleteProductView(PermissionOperator, DeleteView):
     model = Product
     template_name = 'products/products-delete.html'
     success_url = reverse_lazy('products:products')
 
 
-class UpdateProductView(UpdateView):
+class UpdateProductView(PermissionOperator, UpdateView):
     model = Product
     template_name = 'products/products-update.html'
     fields = '__all__'
