b22a3f37b4200c2e7fc897e57812da6dd8791e95
vladimir <vovka1998@gmail.com>
2024-09-19 10:04:38 +0300

    added ListProductView and DetailProductView

--- crm_system/products/urls.py
+++ crm_system/products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import DetailProductView, CreateProductView
+from .views import DetailProductView, CreateProductView, ListProductView
 
 app_name = 'products'
 
 urlpatterns = [
-    path('create/', CreateProductView.as_view(), name='product_create')
+    path('create/', CreateProductView.as_view(), name='products_create'),
+    path('list/', ListProductView.as_view(), name='products_list'),
+    path('detail/<int:pk>/', DetailProductView.as_view(), name='products_detail'),
 ]
--- crm_system/products/views.py
+++ crm_system/products/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import DetailView, CreateView
-from django.shortcuts import render
+from django.views.generic import DetailView, CreateView, DeleteView, UpdateView, ListView
+from django.urls import reverse_lazy
 
 from .models import Product
 
@@ -8,8 +8,15 @@ class CreateProductView(CreateView):
     model = Product
     fields = '__all__'
     template_name = 'products/products-create.html'
+    success_url = reverse_lazy('products:products_list')
 
 
 class DetailProductView(DetailView):
     model = Product
-    template_name = 'products/products-delete.html'
+    template_name = 'products/products-detail.html'
+
+
+class ListProductView(ListView):
+    queryset = Product.objects.all()
+    context_object_name = 'products'
+    template_name = 'products/products-list.html'
