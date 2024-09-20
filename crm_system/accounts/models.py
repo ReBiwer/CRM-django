c3506181d91b80a4a7fe7fc68f4538e8c0128a94
vladimir <vovka1998@gmail.com>
2024-09-20 17:30:02 +0300

    created and fixed model Profile

--- crm_system/accounts/models.py
+++ crm_system/accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
+from typing import TYPE_CHECKING
+from django.db.models import Manager
 
-# Create your models here.
+from products.models import Product
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    second_name = models.CharField(max_length=100, null=True, blank=True)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+
+    if TYPE_CHECKING:
+        objects: Manager
