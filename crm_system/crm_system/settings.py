16b98997d87a1e91be731541b83d3b2d18d8affe
vladimir <vovka1998@gmail.com>
2024-09-20 14:50:42 +0300

    moved base_templates in .crm_system/

--- crm_system/crm_system/settings.py
+++ crm_system/crm_system/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'crm_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
