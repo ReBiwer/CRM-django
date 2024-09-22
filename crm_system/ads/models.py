5d5ece8bfe2f745785457909c1deb4f7661a79c0
vladimir <vovka1998@gmail.com>
2024-09-22 19:38:15 +0300

    created model Ads

--- crm_system/ads/models.py
+++ crm_system/ads/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from products.models import Product
+
+
+class Ads(models.Model):
+    name = models.CharField(max_length=100)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    channel = models.CharField(max_length=100)
+    budget = models.DecimalField(default=0, max_digits=8, decimal_places=2)
