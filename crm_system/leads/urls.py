24972b3492718fcc55831ae461680fa9c60a7a6a
vladimir <vovka1998@gmail.com>
2024-09-23 17:21:07 +0300

    created and connected Update, Delete, Detail views for model Lead

--- crm_system/leads/urls.py
+++ crm_system/leads/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import ListLeadsView, CreateLeadView
+from .views import ListLeadsView, CreateLeadView, DetailLeadView, DeleteLeadView, UpdateLeadView
 
 app_name = 'leads'
 
 urlpatterns = [
-    path('', ListLeadsView.as_view(), name='leads_list'),
+    path('', ListLeadsView.as_view(), name='leads'),
     path('create/', CreateLeadView.as_view(), name='lead_create'),
+    path('detail/<int:pk>/', DetailLeadView.as_view(), name='lead_detail'),
+    path('delete/<int:pk>/', DeleteLeadView.as_view(), name='lead_delete'),
+    path('update/<int:pk>/', UpdateLeadView.as_view(), name='lead_update'),
 ]
--- crm_system/leads/views.py
+++ crm_system/leads/views.py
@@ -9,10 +9,33 @@ class CreateLeadView(CreateView):
     model = Lead
     fields = '__all__'
     template_name = 'leads/leads-create.html'
-    success_url = reverse_lazy('leads:leads_list')
+    success_url = reverse_lazy('leads:leads')
 
 
 class ListLeadsView(ListView):
     queryset = Lead.objects.all()
     context_object_name = 'leads'
     template_name = 'leads/leads-list.html'
+
+
+class DetailLeadView(DetailView):
+    model = Lead
+    template_name = 'leads/leads-detail.html'
+
+
+class DeleteLeadView(DeleteView):
+    model = Lead
+    template_name = 'leads/leads-delete.html'
+    success_url = reverse_lazy('leads:leads')
+
+
+class UpdateLeadView(UpdateView):
+    model = Lead
+    template_name = 'leads/leads-update.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse(
+            'leads:lead_detail',
+            kwargs={'pk': self.object.pk}
+        )
