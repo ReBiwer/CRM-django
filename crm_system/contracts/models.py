2f592a7a9d4508cc576db1d8fe43081e09fec10a
vladimir <vovka1998@gmail.com>
2024-09-24 11:08:19 +0300

    created and migrated model Contact

--- crm_system/contracts/models.py
+++ crm_system/contracts/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from typing import TYPE_CHECKING
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Manager
+
+
+class Contract(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    cost = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    if TYPE_CHECKING:
+        objects: Manager
