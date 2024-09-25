b8818b8fac07cc0b339681db525d9517e472c135
vladimir <vovka1998@gmail.com>
2024-09-25 11:32:36 +0300

    added permissions on views

--- crm_system/contracts/views.py
+++ crm_system/contracts/views.py
@@ -1,47 +1,51 @@
 from django.views.generic import DetailView, CreateView, DeleteView, UpdateView, ListView
 from django.urls import reverse_lazy, reverse
 
-# from crm_system.permissions import PermissionManager
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from .models import Contract
 from .forms import ContractForm
 
 
-class CreateContactView(CreateView):
+class CreateContactView(PermissionRequiredMixin, CreateView):
     form_class = ContractForm
     template_name = 'contracts/contracts-create.html'
     success_url = reverse_lazy('contracts:contracts')
+    permission_required = ['contracts.add_contract']
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
 
 
-class DetailContractView(DetailView):
+class DetailContractView(PermissionRequiredMixin, DetailView):
     model = Contract
     template_name = 'contracts/contracts-detail.html'
+    permission_required = ['contracts.view_contract']
 
 
-class ListContractView(ListView):
+class ListContractView(PermissionRequiredMixin, ListView):
     queryset = Contract.objects.select_related('created_by').all()
     context_object_name = 'contracts'
     template_name = 'contracts/contracts-list.html'
+    permission_required = ['contracts.view_contract']
 
 
-class DeleteContractView(DeleteView):
+class DeleteContractView(PermissionRequiredMixin, DeleteView):
     model = Contract
     template_name = 'contracts/contracts-delete.html'
     success_url = reverse_lazy('contracts:contracts')
+    permission_required = ['contracts.delete_contract']
 
 
-class UpdateContractView(UpdateView):
+class UpdateContractView(PermissionRequiredMixin, UpdateView):
     model = Contract
     form_class = ContractForm
     template_name = 'contracts/contracts-update.html'
+    permission_required = ['contracts.change_contract']
 
     def form_valid(self, form):
         form.instance.created_by = self.object.created_by
         return super().form_valid(form)
-
 
     def get_success_url(self):
         return reverse(
--- crm_system/leads/views.py
+++ crm_system/leads/views.py
@@ -2,37 +2,43 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic import DetailView, CreateView, DeleteView, UpdateView, ListView
 from django.urls import reverse_lazy, reverse
 
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from .models import Lead
 
 
-class CreateLeadView(CreateView):
+class CreateLeadView(PermissionRequiredMixin, CreateView):
     model = Lead
     fields = '__all__'
     template_name = 'leads/leads-create.html'
     success_url = reverse_lazy('leads:leads')
+    permission_required = ['leads.add_lead']
 
 
-class ListLeadsView(ListView):
+class ListLeadsView(PermissionRequiredMixin, ListView):
     queryset = Lead.objects.all()
     context_object_name = 'leads'
     template_name = 'leads/leads-list.html'
+    permission_required = ['leads.view_lead']
 
 
-class DetailLeadView(DetailView):
+class DetailLeadView(PermissionRequiredMixin, DetailView):
     model = Lead
     template_name = 'leads/leads-detail.html'
+    permission_required = ['leads.view_lead']
 
 
-class DeleteLeadView(DeleteView):
+class DeleteLeadView(PermissionRequiredMixin, DeleteView):
     model = Lead
     template_name = 'leads/leads-delete.html'
     success_url = reverse_lazy('leads:leads')
+    permission_required = ['leads.delete_lead']
 
 
-class UpdateLeadView(UpdateView):
+class UpdateLeadView(PermissionRequiredMixin, UpdateView):
     model = Lead
     template_name = 'leads/leads-update.html'
     fields = '__all__'
+    permission_required = ['leads.change_lead']
 
     def get_success_url(self):
         return reverse(
