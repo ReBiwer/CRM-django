9c6f2b73645bc9e67168580fb2b5ea27787c933a
vladimir <vovka1998@gmail.com>
2024-09-25 16:28:36 +0300

    added permissions

--- crm_system/ads/views.py
+++ crm_system/ads/views.py
@@ -5,34 +5,39 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from .models import Ads
 
 
-class CreateAdsView(CreateView):
+class CreateAdsView(PermissionRequiredMixin, CreateView):
     queryset = Ads.objects.select_related('product').all()
     fields = '__all__'
     template_name = 'ads/ads-create.html'
     success_url = reverse_lazy('ads:ads')
+    permission_required = ['ads.add_ads']
 
 
-class DetailAdsView(DetailView):
+class DetailAdsView(PermissionRequiredMixin, DetailView):
     queryset = Ads.objects.select_related('product').all()
     template_name = 'ads/ads-detail.html'
+    permission_required = ['ads.view_ads']
 
 
-class ListAdsView(ListView):
+class ListAdsView(PermissionRequiredMixin, ListView):
     queryset = Ads.objects.all()
     context_object_name = 'ads'
     template_name = 'ads/ads-list.html'
+    permission_required = ['ads.view_ads']
 
 
-class DeleteAdsView(DeleteView):
+class DeleteAdsView(PermissionRequiredMixin, DeleteView):
     model = Ads
     template_name = 'ads/ads-delete.html'
     success_url = reverse_lazy('ads:ads')
+    permission_required = ['ads.delete_ads']
 
 
-class UpdateAdsView(UpdateView):
+class UpdateAdsView(PermissionRequiredMixin, UpdateView):
     model = Ads
     template_name = 'ads/ads-update.html'
     fields = '__all__'
+    permission_required = ['ads.change_ads']
 
     def get_success_url(self):
         return reverse(
