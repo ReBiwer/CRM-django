191a7bb1ccf72c97568569eb847efb794e0a398e
vladimir <vovka1998@gmail.com>
2024-09-26 14:48:21 +0300

    updated model Lead (added field 'ad') and migrated

--- crm_system/leads/models.py
+++ crm_system/leads/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from typing import TYPE_CHECKING
 from django.db.models import Manager
 from phonenumber_field.modelfields import PhoneNumberField
+from ads.models import Ads
 
 
 class Lead(models.Model):
@@ -11,6 +12,7 @@ class Lead(models.Model):
     last_name = models.CharField(max_length=150)
     email = models.EmailField(null=True, blank=True)
     phone = PhoneNumberField(blank=True, region='RU')
+    ad = models.ForeignKey(Ads, on_delete=models.SET_NULL, null=True)
 
     if TYPE_CHECKING:
         objects: Manager
