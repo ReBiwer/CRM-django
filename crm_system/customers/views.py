71c13142406ae80ded76c764dcc2ad0f042c8490
vladimir <vovka1998@gmail.com>
2024-09-26 17:05:17 +0300

    created views

--- crm_system/customers/views.py
+++ crm_system/customers/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, CreateView, DeleteView, UpdateView, ListView
+from django.urls import reverse_lazy, reverse
 
-# Create your views here.
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from .forms import CustomerForm
+from .models import Customer
+
+
+class CreateCustomerView(CreateView):
+    form_class = CustomerForm
+    template_name = 'customers/customers-create.html'
+    success_url = reverse_lazy('customers:customers')
+
+
+class DetailCustomerView(DetailView):
+    queryset = Customer.objects.select_related('ad', 'contract')
+    template_name = 'customers/customers-detail.html'
+
+
+class ListCustomerView(ListView):
+    queryset = Customer.objects.select_related('ad', 'contract').all()
+    context_object_name = 'customers'
+    template_name = 'customers/customers-list.html'
+
+
+class DeleteCustomerView(DeleteView):
+    model = Customer
+    template_name = 'customers/customers-delete.html'
+    success_url = reverse_lazy('customers:customers')
+
+
+class UpdateCustomerView(UpdateView):
+    model = Customer
+    fields = '__all__'
+    template_name = 'customers/customers-update.html'
