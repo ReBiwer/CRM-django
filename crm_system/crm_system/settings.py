23d1c23c86aa68dc14e6ccd7424476ad90bb6aec
vladimir <vovka1998@gmail.com>
2024-09-26 15:31:20 +0300

    added path MEDIA_ROOT

--- crm_system/crm_system/settings.py
+++ crm_system/crm_system/settings.py
@@ -121,6 +121,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'uploads'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
