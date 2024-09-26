b9a8f691be3654d663ce44f0ae277cab551a85c6
vladimir <vovka1998@gmail.com>
2024-09-26 15:30:59 +0300

    added field document and migrated updating model Contract

--- crm_system/contracts/models.py
+++ crm_system/contracts/models.py
@@ -3,10 +3,17 @@ from typing import TYPE_CHECKING
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Manager
+from products.models import Product
+
+
+def contract_directory_path(instance: 'Contract', filename: str) -> str:
+    return 'contracts/contract_{name}/{filename}'.format(name=instance.name, filename=filename)
 
 
 class Contract(models.Model):
     name = models.CharField(max_length=100)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    document = models.FileField(upload_to=contract_directory_path, null=True)
     description = models.TextField(null=True, blank=True)
     cost = models.DecimalField(default=0, max_digits=8, decimal_places=2)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
