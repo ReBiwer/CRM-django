8b4b0a9b54234bc741a437aef0220b1ee1d47b87
vladimir <vovka1998@gmail.com>
2024-09-27 11:17:17 +0300

    added permissions om views

--- crm_system/customers/views.py
+++ crm_system/customers/views.py
@@ -6,30 +6,35 @@ from .forms import CustomerForm
 from .models import Customer
 
 
-class CreateCustomerView(CreateView):
+class CreateCustomerView(PermissionRequiredMixin, CreateView):
     form_class = CustomerForm
     template_name = 'customers/customers-create.html'
     success_url = reverse_lazy('customers:customers')
+    permission_required = ['customers.add_customer']
 
 
-class DetailCustomerView(DetailView):
+class DetailCustomerView(PermissionRequiredMixin, DetailView):
     queryset = Customer.objects.select_related('ad', 'contract')
     template_name = 'customers/customers-detail.html'
+    permission_required = ['customers.view_customer']
 
 
-class ListCustomerView(ListView):
-    queryset = Customer.objects.select_related('ad', 'contract').all()
+class ListCustomerView(PermissionRequiredMixin, ListView):
+    queryset = Customer.objects.all()
     context_object_name = 'customers'
     template_name = 'customers/customers-list.html'
+    permission_required = ['customers.view_customer']
 
 
-class DeleteCustomerView(DeleteView):
+class DeleteCustomerView(PermissionRequiredMixin, DeleteView):
     model = Customer
     template_name = 'customers/customers-delete.html'
     success_url = reverse_lazy('customers:customers')
+    permission_required = ['customers.delete_customer']
 
 
-class UpdateCustomerView(UpdateView):
+class UpdateCustomerView(PermissionRequiredMixin, UpdateView):
     model = Customer
     fields = '__all__'
     template_name = 'customers/customers-update.html'
+    permission_required = ['customers.change_customer']
