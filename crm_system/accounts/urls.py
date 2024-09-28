b4f44e357382a5fce18c8e4a38671856b74a1694
vladimir <vovka1998@gmail.com>
2024-09-28 11:52:20 +0300

    created IndexView and connected url account/index/

--- crm_system/accounts/urls.py
+++ crm_system/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 
-from .views import MyLogoutView
+from .views import MyLogoutView, IndexView
 
 app_name = 'accounts'
 
@@ -15,4 +15,5 @@ urlpatterns = [
         name='login'
     ),
     path('logout/', MyLogoutView.as_view(), name='logout'),
+    path('index/', IndexView.as_view(), name='account_index'),
 ]
--- crm_system/accounts/views.py
+++ crm_system/accounts/views.py
@@ -1,6 +1,25 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, TemplateView
 from django.urls import reverse_lazy
+from products.models import Product
+from ads.models import Ads
+from leads.models import Lead
+from customers.models import Customer
 
 
 class MyLogoutView(LogoutView):
     next_page = reverse_lazy('accounts:login')
+
+
+class IndexView(TemplateView):
+    template_name = 'accounts/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        count_objects = {
+            'products_count': Product.objects.count(),
+            'ads_count': Ads.objects.count(),
+            'leads_count': Lead.objects.count(),
+            'customers_count': Customer.objects.count(),
+        }
+        context = {**count_objects}
+        return context
