b1cd2612ee30208944939ece7fa117407775373c
vladimir <vovka1998@gmail.com>
2024-09-30 12:22:00 +0300

    added statistic for Ads

--- crm_system/ads/urls.py
+++ crm_system/ads/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import ListAdsView, CreateAdsView, DetailAdsView, UpdateAdsView, DeleteAdsView
+from .views import ListAdsView, StatisticAdsView, CreateAdsView, DetailAdsView, UpdateAdsView, DeleteAdsView
 
 app_name = 'ads'
 
 urlpatterns = [
     path('', ListAdsView.as_view(), name='ads'),
+    path('statistic/', StatisticAdsView.as_view(), name='ads_statistic'),
     path('create/', CreateAdsView.as_view(), name='ad_create'),
     path('detail/<int:pk>/', DetailAdsView.as_view(), name='ad_detail'),
     path('update/<int:pk>/', UpdateAdsView.as_view(), name='ad_update'),
--- crm_system/ads/views.py
+++ crm_system/ads/views.py
@@ -1,8 +1,19 @@
-from django.views.generic import DetailView, CreateView, DeleteView, UpdateView, ListView
+from django.views.generic import DetailView, CreateView, DeleteView, UpdateView, ListView, TemplateView
 from django.urls import reverse_lazy, reverse
 
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from .models import Ads
+from functools import reduce
+from decimal import Decimal
+from leads.models import Lead
+from customers.models import Customer
+
+
+class ListAdsView(PermissionRequiredMixin, ListView):
+    queryset = Ads.objects.all()
+    context_object_name = 'ads'
+    template_name = 'ads/ads-list.html'
+    permission_required = ['ads.view_ads']
 
 
 class CreateAdsView(PermissionRequiredMixin, CreateView):
@@ -16,13 +27,6 @@ class CreateAdsView(PermissionRequiredMixin, CreateView):
 class DetailAdsView(PermissionRequiredMixin, DetailView):
     queryset = Ads.objects.select_related('product').all()
     template_name = 'ads/ads-detail.html'
-    permission_required = ['ads.view_ads']
-
-
-class ListAdsView(PermissionRequiredMixin, ListView):
-    queryset = Ads.objects.all()
-    context_object_name = 'ads'
-    template_name = 'ads/ads-list.html'
     permission_required = ['ads.view_ads']
 
 
@@ -44,3 +48,26 @@ class UpdateAdsView(PermissionRequiredMixin, UpdateView):
             'ads:ad_detail',
             kwargs={'pk': self.object.pk}
         )
+
+
+class StatisticAdsView(TemplateView):
+    template_name = 'ads/ads-statistic.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        list_statistic = list()
+        for ad in Ads.objects.all():
+            sum_cost_contracts = 0
+            for customer in Customer.objects.filter(ad=ad.pk):
+                sum_cost_contracts += customer.contract.cost
+            list_statistic.append(
+                (
+                    ad,
+                    Lead.objects.filter(ad=ad.pk).count(),
+                    Customer.objects.filter(ad=ad.pk).count(),
+                    sum_cost_contracts / ad.budget,
+                )
+            )
+        context['statistic'] = list_statistic
+        return context
+
