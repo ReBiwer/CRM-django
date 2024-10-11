4324b93450ed73b72bc6a23b7111a23e8c9d172f
vladimir <vovka1998@gmail.com>
2024-10-11 16:37:04 +0300

    added tests for urls ads app

--- crm_system/ads/tests.py
+++ crm_system/ads/tests.py
@@ -19,11 +19,24 @@ class TestAppAds(TestCase):
             username='marketer',
             password='qwerty123@'
         )
+        cls.test_product = Product.objects.create(
+            name='test_product',
+            description='test_description',
+            cost=Decimal('2999.99'),
+            created_by=cls.test_marketer,
+        )
+        cls.test_ad = Ads.objects.create(
+            name='test_ad',
+            product=cls.test_product,
+            channel='test_channel',
+            budget=29990,
+        )
         cls.test_marketer.groups.add(Group.objects.get(name='Marketer'))
 
     @classmethod
     def tearDownClass(cls):
         cls.test_marketer.delete()
+        cls.test_product.delete()
 
     def setUp(self):
         self.client.force_login(self.test_marketer)
@@ -35,7 +48,7 @@ class TestAppAds(TestCase):
         response = self.client.get(reverse('ads:ads'))
         self.assertEqual(response.status_code, 200)
         for i_ads in Ads.objects.all():
-            with self.subTest(f'Проверка наличия рекламной компании: {i_ads.name}', i_ads=i_ads):
+            with self.subTest(f'Test for the presence ad: {i_ads.name}'):
                 self.assertContains(response, i_ads.name, html=True)
 
     def test_statistic_view(self):
@@ -47,31 +60,84 @@ class TestAppAds(TestCase):
                 sum_cost_contracts += customer.contract.cost
             count_leads = Lead.objects.filter(ad=ad.pk).count()
             count_customers = Customer.objects.filter(ad=ad.pk).count()
-            with self.subTest(f'Проверка наличия статистики на странице', ad=ad):
+            with self.subTest('Test for the presence of advertising statistics'):
                 self.assertContains(response, ad.name, html=True)
                 self.assertContains(response, count_leads)
                 self.assertContains(response, count_customers)
 
-    def test_create_ads_view(self):
-        # response = self.client.get(reverse('ads:ad_create'))
-        # self.assertEqual(response.status_code, 200)
-        test_product = Product.objects.create(
-            name='test_product',
-            description='test_description',
-            cost=Decimal('2999.99'),
-            created_by=self.test_marketer,
+    def test_create_ad_view(self):
+        with self.subTest('Test get method'):
+            response = self.client.get(reverse('ads:ad_create'))
+            self.assertEqual(response.status_code, 200)
+
+        with self.subTest('Test post method'):
+            form_data = {
+                'name': 'new_ad',
+                'product': f'{self.test_product.pk}',
+                'channel': 'someone_channel',
+                'budget': 15000,
+            }
+            response_with_data = self.client.post(
+                reverse('ads:ad_create'),
+                data=form_data,
+                follow=True
+            )
+            self.assertEqual(response_with_data.status_code, 200)
+            self.assertTrue(Ads.objects.filter(name=form_data['name']).exists())
+
+    def test_detail_ad_view(self):
+        response = self.client.get(
+            reverse(
+                'ads:ad_detail',
+                kwargs={'pk': self.test_ad.pk}
+            ),
         )
-        form_data = {
-            'name': 'test_ad',
-            'product': test_product,
-            'channel': 'test_channel',
-            'budget': 15000,
-        }
-        response_with_data = self.client.post(
-            reverse('ads:ad_create'),
-            data=form_data,
-            follow=True
-        )
-        self.assertEqual(response_with_data.status_code, 200)
-        new_ad = Ads.objects.all()
-        self.assertTrue(Ads.objects.get(name='test_ad').exists())
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_ad_view(self):
+        with self.subTest('Test get method'):
+            response = self.client.get(
+                reverse(
+                    'ads:ad_update',
+                    kwargs={'pk': self.test_ad.pk}
+                )
+            )
+            self.assertEqual(response.status_code, 200)
+        with self.subTest('Test post method'):
+            form_data = {
+                'name': 'new_ad',
+                'product': f'{self.test_product.pk}',
+                'channel': 'new_channel',
+                'budget': 15000,
+            }
+            response_with_data = self.client.post(
+                reverse('ads:ad_create'),
+                data=form_data,
+                follow=True
+            )
+            self.assertEqual(response_with_data.status_code, 200)
+            self.assertTrue(Ads.objects.filter(name=form_data['name']).exists())
+
+    def test_delete_ad_view(self):
+        with self.subTest('Test get method'):
+            response = self.client.get(
+                reverse(
+                    'ads:ad_delete',
+                    kwargs={'pk': self.test_ad.pk}
+                )
+            )
+            self.assertEqual(response.status_code, 200)
+        with self.subTest('Test post method'):
+            new_ad = Ads.objects.create(
+                name='test_ad',
+                product=self.test_product,
+                channel='test_channel',
+                budget=29990,
+            )
+            response = self.client.post(
+                reverse(
+                    'ads:ad_delete',
+                    kwargs={'pk': new_ad.pk}
+                )
+            )
+            self.assertEqual(response.status_code, 302)
