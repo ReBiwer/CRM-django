1b9ce59ea8eff695ae3c984c6f0ff7898f9f478f
vladimir <vovka1998@gmail.com>
2024-10-16 14:43:19 +0300

    connected MEADIA_URL

--- crm_system/crm_system/urls.py
+++ crm_system/crm_system/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -26,3 +28,12 @@ urlpatterns = [
     path('contracts/', include('contracts.urls')),
     path('customers/', include('customers.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend(
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
+
+    urlpatterns.extend(
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    )
